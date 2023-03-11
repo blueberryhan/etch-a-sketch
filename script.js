89c88495c9254500255f8ec3f306ad72a70aadb3
Josh <120075423+blueberryhan@users.noreply.github.com>
2023-03-10 19:03:07 -0800

    add 4x4 grid

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const grid = document.querySelector('.container');
 
 for ( i = 0; i <= 15; i++) {
     const gridCell = document.createElement('div');
+    gridCell.classList.add('cell');
     grid.appendChild(gridCell);
 }
 
 console.log(grid);
+
