68ed25017ee92d07b48e4119d78e222eeef61b7f
Josh <120075423+blueberryhan@users.noreply.github.com>
2023-03-10 19:46:26 -0800

    add mechanism to change cell colors to black

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const grid = document.querySelector('.container');
 
-for ( i = 0; i <= 15; i++) {
+for ( i = 0; i <= 255; i++) {
     const gridCell = document.createElement('div');
     gridCell.classList.add('cell');
     grid.appendChild(gridCell);
+
+    gridCell.addEventListener('mouseover', () => {
+        gridCell.style.backgroundColor = 'black';
+    });
 }
 
-console.log(grid);
+
 
