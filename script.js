5c103167a14921db24b474e4f6476edc4fa86ad0
Josh <120075423+blueberryhan@users.noreply.github.com>
2023-03-14 18:41:31 -0700

    add rgb button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('.container');
 
+function defaultGrid() {
+
 grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
     for ( i = 0; i <= 255; i++) {
         const gridCell = document.createElement('div');
@@ -10,32 +12,74 @@ grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
         gridCell.style.backgroundColor = 'black';
         });
     }
+}
 
 function userInputGrid() {
 
-    let gridInput = prompt('Please input grid dimensions');
+    let gridInput = prompt('Please input grid dimensions. MAX: 100');
+
+    if (gridInput === null) {
+        return;
+    }
     grid.innerHTML = '';
+
     grid.style.gridTemplateColumns = `repeat(${gridInput}, 1fr)`;
+
     for ( i = 0; i <= (gridInput * gridInput) - 1; i++) {
-        if (gridInput > 100) {
-            alert('Please input number less than 100 :)');
-            return userInputGrid();
-        }
-        else if (gridInput < 0) {
-            alert('Please input number greater than zero :)');
-            return userInputGrid();
-        }
+        if (gridInput > 100 || gridInput < 0) {
+            alert('Please input valid integer value :)');
+            return defaultGrid();
+        };
+        
         const gridCell = document.createElement('div');
-        gridCell.classList.add('cell-twenty-four');
+        gridCell.classList.add('cell-dimension-input');
         grid.appendChild(gridCell);
         
         gridCell.addEventListener('mouseover', () => {
         gridCell.style.backgroundColor = 'black';
-        });
 
-    }
+        })
+    };
 
 };
 
+function colorChange() {
+    
+    let gridInput = prompt('GAMER MODE ENGAGED, PLS INPUT DIMENSIONS. MAX: 100');
+
+    if (gridInput === null) {
+        return;
+    }
+    grid.innerHTML = '';
+
+    grid.style.gridTemplateColumns = `repeat(${gridInput}, 1fr)`;
+
+    for ( i = 0; i <= (gridInput * gridInput) - 1; i++) {
+        if (gridInput > 100 || gridInput < 0) {
+            alert('Please input valid integer value :)');
+            return defaultGrid();
+        };
+        
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('cell-dimension-input');
+        grid.appendChild(gridCell);
+        
+
+        let randomColor = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+        gridCell.addEventListener('mouseover', () => {
+        gridCell.style.backgroundColor = randomColor;
+        });
+    }
+}
+
 const dimensionButton = document.querySelector('.dimension-button');
 dimensionButton.addEventListener('click', userInputGrid);
+
+defaultGrid();
+
+const colorChangeButton = document.querySelector('.color-change-button');
+colorChangeButton.addEventListener('click', colorChange);
+
+
+
+
