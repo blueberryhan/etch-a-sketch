d0e245955e17d48c7565d8ec80f82b598630e9f4
Josh <120075423+blueberryhan@users.noreply.github.com>
2023-03-15 18:20:13 -0700

    add colors and layout change

--- script.js
+++ script.js
@@ -3,13 +3,19 @@ const grid = document.querySelector('.container');
 function defaultGrid() {
 
 grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
+
     for ( i = 0; i <= 255; i++) {
+
         const gridCell = document.createElement('div');
+
         gridCell.classList.add('cell-sixteen');
+
         grid.appendChild(gridCell);
 
         gridCell.addEventListener('mouseover', () => {
+
         gridCell.style.backgroundColor = 'black';
+
         });
     }
 }
@@ -26,21 +32,27 @@ function userInputGrid() {
     grid.style.gridTemplateColumns = `repeat(${gridInput}, 1fr)`;
 
     for ( i = 0; i <= (gridInput * gridInput) - 1; i++) {
+
         if (gridInput > 100 || gridInput < 0) {
+
             alert('Please input valid integer value :)');
+
             return defaultGrid();
+
         };
         
         const gridCell = document.createElement('div');
+
         gridCell.classList.add('cell-dimension-input');
+
         grid.appendChild(gridCell);
         
         gridCell.addEventListener('mouseover', () => {
+
         gridCell.style.backgroundColor = 'black';
 
         })
     };
-
 };
 
 function colorChange() {
@@ -55,29 +67,39 @@ function colorChange() {
     grid.style.gridTemplateColumns = `repeat(${gridInput}, 1fr)`;
 
     for ( i = 0; i <= (gridInput * gridInput) - 1; i++) {
+
         if (gridInput > 100 || gridInput < 0) {
+
             alert('Please input valid integer value :)');
+
             return defaultGrid();
+
         };
         
         const gridCell = document.createElement('div');
+
         gridCell.classList.add('cell-dimension-input');
+
         grid.appendChild(gridCell);
-        
 
         let randomColor = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+
         gridCell.addEventListener('mouseover', () => {
+
         gridCell.style.backgroundColor = randomColor;
+
         });
     }
 }
 
 const dimensionButton = document.querySelector('.dimension-button');
+
 dimensionButton.addEventListener('click', userInputGrid);
 
 defaultGrid();
 
 const colorChangeButton = document.querySelector('.color-change-button');
+
 colorChangeButton.addEventListener('click', colorChange);
 
 
